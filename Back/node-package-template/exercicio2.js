4e6129767a3e92273a1848ae4165462eb5457d1b
Rafael Orlando <0501.orlando@gmail.com>
2021-05-17 16:40:59 -0300

    exercicio node

---
+++ Back/node-package-template/exercicio2.js
@@ -0,0 +1,13 @@
+//Exercicio 2
+
+const somar = 2 + 2;
+console.log(`Resposta : ${somar}`)
+
+const subtrair = 10 - 2;
+console.log(`Resposta : ${subtrair}`)
+
+const multiplicar = 50 * 2;
+console.log(`resposta : ${multiplicar}`)
+
+const dividir = 100 / 2;
+console.log(`Resposta : ${dividir}`)
